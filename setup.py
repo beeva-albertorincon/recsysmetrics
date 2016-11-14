9e903cb9341a6d68e0ee1620a4d862d90c2daf43
Alberto Rincón <alberto.rincon@beeva.com>
2016-11-14 10:07:57 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+ #!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='recsysmetrics',
+      version='0.1',
+      description='Metrics for recommendation systems.',
+      author='beeva_labs',
+      author_email='research.lab@beeva.com',
+      url='',
+      py_modules=['metrics.MAPTest'],
+     )
